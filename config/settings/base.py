677cc4adfe3042170d98440ad927c0c5beb61b37
daceroso <daceros19@gmail.com>
2025-02-02 17:15:31 -0500

    chore: setup makefile tasks

--- config/settings/base.py
+++ config/settings/base.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "medium_api.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "medium_api.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
